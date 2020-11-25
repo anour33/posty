fda0a85ed78d363b755db5d66efd1ce98dc90a4a
abdenour <anouramarouch@gmail.com>
2020-11-25 01:28:55 +0100

    CRUD1

--- store.py
+++ store.py
@@ -1,15 +1,45 @@
-class post:
-
-    def __init__(self,name,image,content) :
+class Post:
+    def __init__(self, id, photo_url, name, body):
+        self.id = id
+        self.photo_url = photo_url
         self.name = name
-        self.image = image
-        self.content = content
+        self.body = body
+
+posts = []
+
+class PostStore:
+    def get_all(self):
+        for post in posts :
+
+            return posts
+
+    def add(self, post):
+        self.get_all()
+        posts.append(post)
+
+    def get_by_id(self, id):
+        self.get_all()
+        for post in posts:
+           if id == post.id:
+                return post
+        else:
+              return None
 
 
-post1 = post(name="Yaser",image="img_url",content="je suis Yaser ,et ceci est un exemple de post ")
-post2 = post(name="Mohamed",image="img_url",content="je suis Mohamed,et ceci est un exemple de post ")
-post3 = post(name="Sara",image="img_url",content="je suis Sara ,et ceci est un exemple de post ")
 
-print(post1.name,"\n",post1.content)
-print(post2.name,"\n",post2.content)
-print(post3.name,"\n",post3.content)
+
+
+post_store = PostStore()
+post1=Post(id=1 , photo_url="photo_url" , name="salim",body="body" )
+post2=Post(id=2 , photo_url="photo_url" , name="sara",body="body" )
+post3=Post(id=3 , photo_url="photo_url" , name="samir",body="body" )
+
+post_store.add(post1)
+post_store.add(post2)
+post_store.add(post3)
+
+mes_posts = post_store.get_all()
+print(mes_posts)
+mon_post=post_store.get_by_id(1)
+print(mon_post)
+
